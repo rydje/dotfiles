eac33750c92f2e61e0e1ec47c7c7d91f9acb0dd2
Ryan Djebrouni <ryan.djebrouni@gmail.com>
2023-03-27 17:23:21 +0200

    reset emacs config

--- emacs.d/elpa/archive-downloader-1.1/archive-downloader-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from archive-downloader.el  -*- no-byte-compile: t -*-
-(define-package "archive-downloader" "1.1" "Download files from archive.org" 'nil)
--- emacs.d/elpa/material-theme-2015/material-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from material-theme.el  -*- no-byte-compile: t -*-
-(define-package "material-theme" "2015" "A Theme based on the colors of the Google Material Design" '((emacs "24.1")) :authors '(("Christoph Paulik" . "cpaulik@gmail.com")) :maintainer '("Christoph Paulik" . "cpaulik@gmail.com") :keywords '("themes") :url "http://github.com/cpaulik/emacs-material-theme")
